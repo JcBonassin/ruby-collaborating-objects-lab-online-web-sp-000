c0ba6a4a2c11eb8b2805bfa021d9c942507ca3e9
JC <bonassin@gmail.com>
2021-01-08 16:25:56 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,5 +5,9 @@ class MP3Importer
     @path = path
   end
 
+  def file
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+  end
 
+    # ||= is a ruby idiom. It means if @current_user is nil (or false) then try to find it by id and assign it to @current_user, otherwise do nothing.
 end
