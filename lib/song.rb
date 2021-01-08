460d2e282f69f4ca8e97cb9226d4f99c0e815c6a
JC <bonassin@gmail.com>
2021-01-08 17:15:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ end
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
-    new_song.artist_name = artist
+    new_song.artist_name == artist
     new_song.save
   end
 
